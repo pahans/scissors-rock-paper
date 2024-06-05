3290184ec8d173937c5173211f672036c1d7f161
Pahan Sarathchandra <pahan123@gmail.com>
2024-06-06 01:05:59 +0300

    feat: indicate computer and player choices clearly

--- src/components/GameStatus/PlayingStage.tsx
+++ src/components/GameStatus/PlayingStage.tsx
@@ -4,7 +4,11 @@ export const PlayingStage: React.FC<{
   selectedChoices: string;
   computerChoice: GameChoice | null;
 }> = ({ selectedChoices, computerChoice }) => (
-  <h3 className="text-3xl">
-    {selectedChoices} vs {computerChoice}
+  <h3 className="text-5xl uppercase text-white">
+    <span className="px-8 text-lg">Computer Choice</span>
+    {computerChoice}
+    <span className="px-8 text-xl text-gold-500">vs</span>
+    {selectedChoices}
+    <span className="px-8 text-lg">Player Choice</span>
   </h3>
 );
